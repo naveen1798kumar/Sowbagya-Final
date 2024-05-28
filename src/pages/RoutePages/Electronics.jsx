65a8afe54d7991955c8d1c43d832054e1704e868
naveen1798kumar <naveennaveennk1798@gmail.com>
2024-05-28 12:28:02 +0530

    electronics updated

--- src/pages/RoutePages/Electronics.jsx
+++ src/pages/RoutePages/Electronics.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import Electronic from "../../pages/ElectronicsSec"
 
 const Electronics = () => {
   return (
     <div className="min-h-screen pt-14 bg-gray-100">
-      <h1 className='text-center pt-14 font-bold' >Opening Soon</h1>
-      {/* <BlogsComp /> */}
+      {/* <h1 className='text-center pt-14 font-bold' >Opening Soon</h1> */}
+      <Electronic />
     </div>
   );
 }
